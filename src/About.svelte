8c752d68b059fad68099e6150f132d304ee93649
Stefanie Hetjens <stefanie@hetjens.com>
2020-03-17 12:15:11 +0100

    typo

--- src/About.svelte
+++ src/About.svelte
@@ -40,7 +40,7 @@
 
     <p>What does it mean to you that 7.7 billion people currently live on our planet? Can you grasp this number? Did you know that only 2.5 billion people lived in 1950? Do you appreciate how vast the growth rates of the global population were in the last years?</p>
 
-    <p>It's relatively easy to repeat these numbers and rates, but it's hard to understand them deeply. If we don't practice, our brains work on logarithmic scales: We find it hard to imagine that one billion is a thousand  times one million. My goal is with this art project is to make it easier for us to visualize the world's population and its growth.</p>
+    <p>It's relatively easy to repeat these numbers and rates, but it's hard to understand them deeply. If we don't practice, our brains work on logarithmic scales: We find it hard to imagine that one billion is a thousand  times one million. My goal with this art project is to make it easier for us to visualize the world's population and its growth.</p>
 
     <p>Depending on the display of your phone or computer, one emoji converts to roughly 3 - 40 million people. 3 million is still a big number, and it’s hard work to process these numbers. I want to invite you to translate these numbers into something you know, like a nearby city or a whole country. Then go on a journey and explore the differences between regions, genders, age groups, and their change over time. Finally, be amazed and proud of how we transformed.</p>
 
